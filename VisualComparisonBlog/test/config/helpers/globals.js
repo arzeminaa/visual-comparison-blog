1fed675cbac3a034a00b3675e8ccdf9b59a3f25d
Arzemina - Fakultet <arzemina-fakultet@Arzeminas-MacBook-Pro-2019.local>
2023-11-04 22:22:17 +0100

    Minor fix

--- VisualComparisonBlog/test/config/helpers/globals.js
+++ VisualComparisonBlog/test/config/helpers/globals.js
@@ -8,20 +8,3 @@ global.responsive = process.env.RESPONSIVE === 'undefined' ? undefined : process
 global.capturePageSource = process.env.CAPTURE_PAGE_SOURCE === 'true' ? true : false
 global.captureScreenshot = process.env.CAPTURE_SCREENSHOT === 'true' ? true : false
 
-//Helper method for blocking wait
-global.pause = time => {
-    return new Promise(resolve => {
-        console.log(`Waiting for ${time} ms`)
-        setTimeout(resolve, time)
-    })
-}
-
-global.sleep = time => {
-    return new Promise(resolve => {
-        console.log(`Waiting for ${time} ms`)
-        setTimeout(resolve, time)
-    })
-}
-
-global.clone = (...sources) =>
-    sources.reduce((obj, source) => Object.assign(obj, JSON.parse(JSON.stringify(source))), {})
